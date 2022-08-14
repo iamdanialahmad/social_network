b737c8e0656fac52a09879d1e22f37a1fe659384
Danial Ahmad <108906119+iamdanialahmad@users.noreply.github.com>
2022-08-14 07:13:48 +0500

    Stretch Goals: Stripe Integration And Paywall
    WHAT: Stripe integration and paywall on social feed
    
    WHY: Social feed restricted to only subscribed users
    
    IMPACT : MID

--- controllers/feed.js
+++ controllers/feed.js
@@ -7,22 +7,26 @@ const userFeed = async (req, res) => {
     const { page = 1, limit = 2 } = req.query;
     const currUser = await User.findById(req.userId);
 
-    // get total documents in the Posts collection
-    const totalItems = await Post.find({ userId: { $in: currUser.following } }).countDocuments();
+    if (!currUser.isPaid) {
+      res.status(403).json('you are not a premium member ');
+    } else {
+      // get total documents in the Posts collection
+      const totalItems = await Post.find({ userId: { $in: currUser.following } }).countDocuments();
 
-    // execute query with page and limit values
-    const posts = await Post.find({ userId: { $in: currUser.following } })
-      .limit(limit * 1)
-      .skip((page - 1) * page)
-      .sort({ createdAt: -1 })
-      .select('-reported -updatedAt -__v')
-      .exec();
+      // execute query with page and limit values
+      const posts = await Post.find({ userId: { $in: currUser.following } })
+        .limit(limit * 1)
+        .skip((page - 1) * page)
+        .sort({ createdAt: -1 })
+        .select('-reported -updatedAt -__v')
+        .exec();
       // return response with posts, total pages, and current page
-    res.status(200).json({
-      posts,
-      totalPages: Math.ceil(totalItems / limit),
-      CurrentPage: page,
-    });
+      res.status(200).json({
+        posts,
+        totalPages: Math.ceil(totalItems / limit),
+        CurrentPage: page,
+      });
+    }
   } catch (error) {
     res.status(500).json(`Error: ${error}`);
   }
